900541d8b9450987d8cb9013aa0a971b08d5bbd1
NimfaMargo <Nikiforova.Rita@gmail.com>
2023-04-30 23:01:13 +0300

    rename env

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
-import { config } from '../config/default.js'
 import { ogg } from './oog.js'
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
-
+const bot = new Telegraf(process.env.GPT_BOT_TOKEN)
+  
 bot.command('start', async (ctx) => {
 	await ctx.reply(JSON.stringify(ctx.message, null, 2));
 })
@@ -15,6 +14,9 @@ bot.on(message('voice'), async (ctx) => {
 		const userId = String(ctx.message.from.id);
 		const oggPath = await ogg.create(link.href, userId);
 		const mp3Path = await ogg.toMp3(oggPath, userId)
+
+
+		const text = await OpenAI.t
 		await ctx.reply(JSON.stringify(mp3Path, null, 2));
 	} catch (e) {
 		console.error(e)
